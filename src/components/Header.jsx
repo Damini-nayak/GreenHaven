dd62226d1a3945ea4660be9072d45bd5708cc061
Damini-nayak <100026489+Damini-nayak@users.noreply.github.com>
2024-12-14 18:48:43 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState} from 'react'
 import Base from './Base'
+import '../style/App.css'
 
 
 export default function Header() {
 
-  useEffect(() => {
-    const menu = document.getElementsByClassName('menu');
-    const dropdown = document.getElementsByClassName('dropdown');
-    menu.addEventListener('click', () => {
-      if (display === 'hidden') {
-        dropdown.style.display = "visible";
-      } else {
-        dropdown.style.display = "hidden";
-      }
-    })
-  })
+ const [isDropdownVisible, setIsDropDownVisible] = useState(false);
+
+ const dropdownToggle = () => {
+  setIsDropDownVisible((prevState) => !prevState);
+ };
 
   return (
     <>
       <div className='header'>
-        <div className='menu'><div className='dropicon'></div>Menu</div>
-        <div className='dropdown'><a>Shop</a><a>Contact us</a><a>Plant care guides</a><a>DIY's</a><a>Garden design ideas</a><a>Showcase</a></div>
+        <div className='menu'><div className='dropicon' onClick={dropdownToggle}></div>Menu</div>
+        <div className={`dropdown ${isDropdownVisible ? 'visible' : 'hidden'}`}><a>Shop</a><a>Contact us</a><a>Plant care guides</a><a>DIY's</a><a>Garden design ideas</a><a>Showcase</a></div>
         <div className='logo'><img src={Base.logo} alt="" />GreenHaven</div>
         <div className='login'><button>Login</button></div>
       </div>
